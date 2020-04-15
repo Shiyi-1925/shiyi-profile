50d68a873f647711f5c80cba1b65b800aff0dd5b
shiyi <shiyi.chen.138@gmail.com>
2020-04-15 20:26:07 +0800

    V1.5

--- src/Pages/PageDesign.js
+++ src/Pages/PageDesign.js
@@ -3,28 +3,28 @@ import Site from './CvModules/Site';
 import Greener from './CvModules/Greener';
 import RubbishBin from './CvModules/RubbishBin';
 import Badmin from './CvModules/Badmin';
-
 import { 
-    Timeline,
+    Timeline
   } from 'antd';
+
 
 class PageDesign extends React.Component{  
     render() {
       return (       
-        <>  
+        <Anchor>  
             <div className="site-layout-background" style={{ padding: 24, textAlign: 'left' }}>
               <Timeline mode='left'>
-                <Timeline.Item label="2020.03"><a href="#site">Mécatronique 网站设计</a></Timeline.Item>
-                <Timeline.Item label="2019.10"><a href="#greener">Go Greener 概念生态农场</a></Timeline.Item>
-                <Timeline.Item label="2018.07"><a href="#rubbishbin">公共垃圾桶设计</a></Timeline.Item>
-                <Timeline.Item label="2016.09"><a href="#badmin">同济大学羽毛球协会宣传品设计</a></Timeline.Item>
+                <Timeline.Item label="2020.03">Mécatronique 网站设计</Timeline.Item>
+                <Timeline.Item label="2019.10">Go Greener 概念生态农场</Timeline.Item>
+                <Timeline.Item label="2018.07">公共垃圾桶设计</Timeline.Item>
+                <Timeline.Item label="2016.09">同济大学羽毛球协会宣传品设计</Timeline.Item>
               </Timeline>
             </div>                    
           <Site/>
           <Greener/>
           <RubbishBin/>
           <Badmin/>
-        </>
+        </Anchor>
       )
     }
 }
