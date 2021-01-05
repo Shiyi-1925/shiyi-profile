e70239624f1c3a3e2668aae4c8dd97bb6528e3fc
Shiyi-1925 <shiyi.chen.138@gmail.com>
2021-01-05 22:14:47 +0100

    V2.1 Unity WebGL added

--- src/Pages/CvModules/Bounce.js
+++ src/Pages/CvModules/Bounce.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import intl from 'react-intl-universal';
+import Unity, { UnityContext } from "react-unity-webgl";
+
 import {
   Typography,
   Col,
@@ -7,12 +9,18 @@ import {
   Card
 } from 'antd';
 
-
 const { Title, Paragraph } = Typography;
 
 const { Meta } = Card;
 
 const UnityGameAddress = "https://play.unity.com/mg/other/untitled-33855";
+
+const unityContext = new UnityContext({
+  loaderUrl: "./Build/BounceBounce.loader.js",
+  dataUrl: "./Build/BounceBounce.data",
+  frameworkUrl: "./Build/BounceBounce.framework.js",
+  codeUrl: "./Build/BounceBounce.wasm",
+});
 
 class Bounce extends React.Component {
   render() {
@@ -30,12 +38,13 @@ class Bounce extends React.Component {
 
         <Row gutter={[16, 16]} justify="center">
           <Col span={16}>
-            <Card cover={<img src={require('../../img/Bounce_screenshot.png')} alt="" />}>
+            {/* <Card cover={<img src={require('../../img/Bounce_screenshot.png')} alt="" />}>
                 <Meta title={intl.get('BOUNCE_DESC3')} style={{ textAlign: "center" }} 
                     description={<a href={UnityGameAddress} target="_blank" rel="noopener noreferrer">{UnityGameAddress}</a>}/>
-            </Card>
+            </Card> */}
+            <Unity unityContext={unityContext} />
           </Col>
-        </Row>
+        </Row>     
       </div>
     )
   }
